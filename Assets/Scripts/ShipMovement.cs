1ffbc5bb4171d0fb59b0c25ae04846aebf6effa0
AllanWanted <estudiante06allan@gmail.com>
2021-10-26 13:54:33 -0600

    13.54
    La nave tiene movimiento

---
+++ Assets/Scripts/ShipMovement.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// seguridad por si olvida el rigidbody, crea un restricion para que no sea eliminado
+[RequireComponent(typeof(Rigidbody))]
+public class ShipMovement : MonoBehaviour
+{
+    public float Velocity = 8;
+
+    Rigidbody rig;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rig = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 DirectionX = Input.GetAxis ("Horizontal") *  Vector3.right;
+        Vector3 DirectionZ = Input.GetAxis("Vertical") * Vector3.forward;
+
+        Vector3 Direction = DirectionX + DirectionZ;
+        Vector3 VectorVelocity = Direction * Velocity;
+
+        rig.velocity = VectorVelocity;
+
+    }
+}
